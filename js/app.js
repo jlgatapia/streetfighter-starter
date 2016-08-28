0903b8af59f8d78a38fd5857aba66f3d11abcffb
Patrick Ford <patrickford@msn.com>
2016-08-28 11:30:06 -0700

    Fixed keyCode. Some indentation.

--- js/app.js
+++ js/app.js
@@ -1,52 +1,62 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
+    $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();    
     $('.ryu-ready').show();
   })
   .mouseleave(function() {
     $('.ryu-ready').hide();
+    $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();    
     $('.ryu-still').show();
   })
   .mousedown(function() {
     playHadouken();
     // play hadouken sound
     $('.ryu-ready').hide();
+    $('.ryu-cool').hide();    
+    $('.ryu-still').hide();    
     $('.ryu-throwing').show();
     $('.hadouken').show()
     // animate hadouken to the right of the screen
-  $('.hadouken').finish().show()
-  .animate(
-    {'left': '1020px'},
-    500,
-    function() {
-      $('.hadouken').hide();
-      $('.hadouken').css('left', '520px');
-    }
-  );
-
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '1020px'},
+      500,
+      function() {
+        $('.hadouken').hide();
+        $('.hadouken').css('left', '520px');
+      }
+    );
   })
   .mouseup(function() {
+    $('.ryu-cool').hide();    
+    $('.ryu-still').hide();        
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
 
-$(document).keydown(function(88) {
+  $(document).keydown(function(e) {
+    if (e.keyCode === 88) {
       $('.ryu-still').hide();
       $('.ryu-ready').hide();
       $('.ryu-throwing').hide();
       $('.ryu-cool').show();
+    }
   })  
-    .keyup(function(88) {
+  .keyup(function(e) {
+    if (e.keyCode === 88) {      
       $('.ryu-cool').hide();
       $('.ryu-still').hide();
       $('.ryu-throwing').hide();
       $('.ryu-still').show();
-   }
-  );
+    }
+  });
 
 });
 
-function playHadouken () {
+function playHadouken() {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
